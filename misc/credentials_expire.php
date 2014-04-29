0d62c60adb5d7e6451f01706691223d3aaf8c274
Bas Zoetekouw <bas@zoetekouw.net>
2014-04-29 14:05:25 +0200

    don't be verbose in cron jobs

--- misc/credentials_expire.php
+++ misc/credentials_expire.php
@@ -5,7 +5,7 @@ require_once( dirname(__FILE__) . '/../lib/credentials.php' );
 
 $num = credential_expire();
 
-print "Expired $num credentials from db\n";
+#print "Expired $num credentials from db\n";
 exit(0);
 
 ?>
